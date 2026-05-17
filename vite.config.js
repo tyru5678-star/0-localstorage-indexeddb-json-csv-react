38704ac5fb3fc3ff5272899e75f6adf3ddfb5667
tyru5678-star <tyru5678@gmail.com>
2026-05-17 19:37:11 +0900

    GitHub Pages 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/teacher-schedule-helper/',
+  base: '/0-localstorage-indexeddb-json-csv-react/',
   plugins: [react()],
 });
